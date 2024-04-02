ca97a89194513173ddb46ad8cc299e87683c0b30
dhava <dhava@HP>
2024-04-02 09:28:16 +0530

    TNSFirst

---
+++ TNS/TNSproject/src/com/tns/dayone/Firstdayone.java
@@ -0,0 +1,9 @@
+package com.tns.dayone;
+
+public class Firstdayone {
+	public static void main(String [] args)
+	{
+		System.out.println("hello tns");
+	}
+
+}
---
+++ TNS/TNSproject/src/com/tns/dayone/datatypedemo.java
@@ -0,0 +1,18 @@
+package com.tns.dayone;
+
+public class datatypedemo {
+	
+	public static void main(String [] args)
+	{
+		int value1=9/12;
+		
+		double value2=10d/6d;
+		float value3=5/2.5f;
+						
+		System.out.println("value1"+value1);
+		System.out.println("value2"+value2);
+		System.out.println("value3"+value3);
+		
+	}
+
+}
